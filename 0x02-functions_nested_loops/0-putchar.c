0622da0fba01d6095559ab63d2416736a21a02ec
norber01 <dovinorbert@gmail.com>
2023-02-21 06:00:49 -0800

    first0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
-#include "main.h"
+#include"main.h"
 int main(void)
 {
-	char str[] = "_putcher";
+	char str[] = "_putchar";
 	int ch;
-for (ch = 0; ch < 8; ++ch)
+
+	for (ch = 0; ch < 8; ++ch)
 		_putchar(str[ch]);
 	_putchar('\n');
 
 	return (0);
-}
+} 
