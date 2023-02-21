72ad798b687100c0b0eca2f2bebc76420c420fef
norber01 <dovinorbert@gmail.com>
2023-02-21 06:26:48 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * main - Entry point
@@ -7,15 +7,14 @@
  *
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
+    char str[] = "_putchar";
+    int ch;
 
-	for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
-	_putchar('\n');
+    for (ch = 0; ch < 8; ++ch)
+        _putchar(str[ch]);
+    _putchar('\n');
 
-	return (0);
+    return (0);
 } 
