3222f630950c363a76786e7d36e5689ae5a7fabe
norber01 <dovinorbert@gmail.com>
2023-02-27 03:57:35 -0800

    task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,16 @@
 #include "main.h"
-/**
- * _islower - check if a character is a
- * lowercase in English alphabet.
- * @c: character to test.
- * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
- */
-int _islower(int c)
-{
-if (c >= 'a' && c <= 'z')
-return (1);
-else
-return (0);
 
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
